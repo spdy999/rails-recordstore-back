3850598f73928643e50bb0461a594251d81ffa21
avenger999 <1a1a1a.262626.0010@gmail.com>
2019-12-11 02:07:18 +0700

    rails g scaffold Artist name:string user:references

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_10_185312) do
+ActiveRecord::Schema.define(version: 2019_12_10_190110) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_artists_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
